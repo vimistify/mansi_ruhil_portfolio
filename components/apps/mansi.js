03078579e557070103522e1a7a73cecc2394e913
Mansi Ruhil <157950124+vimistify@users.noreply.github.com>
2025-03-27 21:14:44 +0530

    Add files via upload

---
+++ components/apps/mansi.js
@@ -0,0 +1,368 @@
+import React, { Component } from 'react';
+import ReactGA from 'react-ga4';
+
+export class AboutMansi extends Component {
+
+    constructor() {
+        super();
+        this.screens = {};
+        this.state = {
+            screen: () => { },
+            active_screen: "about",
+            navbar: false,
+        }
+    }
+
+    componentDidMount() {
+        this.screens = {
+            "about": <About />,
+            "education": <Education />,
+            "skills": <Skills />,
+            "projects": <Projects />,
+            "resume": <Resume />,
+        }
+
+        let lastVisitedScreen = localStorage.getItem("about-section");
+        if (lastVisitedScreen === null || lastVisitedScreen === undefined) {
+            lastVisitedScreen = "about";
+        }
+
+        // focus last visited screen
+        this.changeScreen(document.getElementById(lastVisitedScreen));
+    }
+
+    changeScreen = (e) => {
+        const screen = e.id || e.target.id;
+
+        // store this state
+        localStorage.setItem("about-section", screen);
+
+        // google analytics
+        ReactGA.send({ hitType: "pageview", page: `/${screen}`, title: "Custom Title" });
+
+
+        this.setState({
+            screen: this.screens[screen],
+            active_screen: screen
+        });
+    }
+
+    showNavBar = () => {
+        this.setState({ navbar: !this.state.navbar });
+    }
+
+    renderNavLinks = () => {
+        return (
+            <>
+                <div id="about" tabIndex="0" onFocus={this.changeScreen} className={(this.state.active_screen === "about" ? " bg-ub-orange bg-opacity-100 hover:bg-opacity-95" : " hover:bg-gray-50 hover:bg-opacity-5 ") + " w-28 md:w-full md:rounded-none rounded-sm cursor-default outline-none py-1.5 focus:outline-none duration-100 my-0.5 flex justify-start items-center pl-2 md:pl-2.5"}>
+                    <img className=" w-3 md:w-4" alt="about vivek" src="./themes/Yaru/status/about.svg" />
+                    <span className=" ml-1 md:ml-2 text-gray-50 ">About Me</span>
+                </div>
+                <div id="education" tabIndex="0" onFocus={this.changeScreen} className={(this.state.active_screen === "education" ? " bg-ub-orange bg-opacity-100 hover:bg-opacity-95" : " hover:bg-gray-50 hover:bg-opacity-5 ") + " w-28 md:w-full md:rounded-none rounded-sm cursor-default outline-none py-1.5 focus:outline-none duration-100 my-0.5 flex justify-start items-center pl-2 md:pl-2.5"}>
+                    <img className=" w-3 md:w-4" alt="vivek' education" src="./themes/Yaru/status/education.svg" />
+                    <span className=" ml-1 md:ml-2 text-gray-50 ">Education</span>
+                </div>
+                <div id="skills" tabIndex="0" onFocus={this.changeScreen} className={(this.state.active_screen === "skills" ? " bg-ub-orange bg-opacity-100 hover:bg-opacity-95" : " hover:bg-gray-50 hover:bg-opacity-5 ") + " w-28 md:w-full md:rounded-none rounded-sm cursor-default outline-none py-1.5 focus:outline-none duration-100 my-0.5 flex justify-start items-center pl-2 md:pl-2.5"}>
+                    <img className=" w-3 md:w-4" alt="vivek' skills" src="./themes/Yaru/status/skills.svg" />
+                    <span className=" ml-1 md:ml-2 text-gray-50 ">Skills</span>
+                </div>
+                <div id="projects" tabIndex="0" onFocus={this.changeScreen} className={(this.state.active_screen === "projects" ? " bg-ub-orange bg-opacity-100 hover:bg-opacity-95" : " hover:bg-gray-50 hover:bg-opacity-5 ") + " w-28 md:w-full md:rounded-none rounded-sm cursor-default outline-none py-1.5 focus:outline-none duration-100 my-0.5 flex justify-start items-center pl-2 md:pl-2.5"}>
+                    <img className=" w-3 md:w-4" alt="vivek' projects" src="./themes/Yaru/status/projects.svg" />
+                    <span className=" ml-1 md:ml-2 text-gray-50 ">Projects</span>
+                </div>
+                <div id="resume" tabIndex="0" onFocus={this.changeScreen} className={(this.state.active_screen === "resume" ? " bg-ub-orange bg-opacity-100 hover:bg-opacity-95" : " hover:bg-gray-50 hover:bg-opacity-5 ") + " w-28 md:w-full md:rounded-none rounded-sm cursor-default outline-none py-1.5 focus:outline-none duration-100 my-0.5 flex justify-start items-center pl-2 md:pl-2.5"}>
+                    <img className=" w-3 md:w-4" alt="vivek's resume" src="./themes/Yaru/status/download.svg" />
+                    <span className=" ml-1 md:ml-2 text-gray-50 ">Resume</span>
+                </div>
+                <div className='my-0.5 w-28 md:w-full h-8 px-2 md:px-2.5 flex' >
+                    <iframe src="https://github.com/sponsors/vimistify/button" title="Sponsor vivek9patel" width={"100%"} height={"100%"} ></iframe>
+                </div>
+            </>
+        );
+    }
+
+    render() {
+        return (
+            <div className="w-full h-full flex bg-ub-cool-grey text-white select-none relative">
+                <div className="md:flex hidden flex-col w-1/4 md:w-1/5 text-sm overflow-y-auto windowMainScreen border-r border-black">
+                    {this.renderNavLinks()}
+                </div>
+                <div onClick={this.showNavBar} className="md:hidden flex flex-col items-center justify-center absolute bg-ub-cool-grey rounded w-6 h-6 top-1 left-1">
+                    <div className=" w-3.5 border-t border-white"></div>
+                    <div className=" w-3.5 border-t border-white" style={{ marginTop: "2pt", marginBottom: "2pt" }}></div>
+                    <div className=" w-3.5 border-t border-white"></div>
+                    <div className={(this.state.navbar ? " visible animateShow z-30 " : " invisible ") + " md:hidden text-xs absolute bg-ub-cool-grey py-0.5 px-1 rounded-sm top-full mt-1 left-0 shadow border-black border border-opacity-20"}>
+                        {this.renderNavLinks()}
+                    </div>
+                </div>
+                <div className="flex flex-col w-3/4 md:w-4/5 justify-start items-center flex-grow bg-ub-grey overflow-y-auto windowMainScreen">
+                    {this.state.screen}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AboutMansi;
+
+export const displayAboutMansi = () => {
+    return <AboutMansi />;
+}
+
+
+function About() {
+    return (
+        <>
+            <div className="w-20 md:w-28 my-4 bg-white rounded-full">
+                <img className="w-full" src="./images/logos/bitmoji.png" alt="Logo" />
+            </div>
+            <div className=" mt-4 md:mt-8 text-lg md:text-2xl text-center px-1">
+                <div>my name is <span className="font-bold">Mansi Ruhil</span> </div>
+                <div className="font-normal ml-1">I'm a <span className="text-pink-600 font-bold">Frontend Developer</span></div>
+            </div>
+            <div className=" mt-4 relative md:my-8 pt-px bg-white w-32 md:w-48">
+                <div className="bg-white absolute rounded-full p-0.5 md:p-1 top-0 transform -translate-y-1/2 left-0"></div>
+                <div className="bg-white absolute rounded-full p-0.5 md:p-1 top-0 transform -translate-y-1/2 right-0"></div>
+            </div>
+            <ul className=" mt-4 leading-tight tracking-tight text-sm md:text-base w-5/6 md:w-3/4 emoji-list">
+                <li>● I'm a <span className=" font-medium">Computer Science Student graduating in 2027. </span> <br></br>( Hit me up <a className='text-underline' href='mailto:mansiruhil99@gmail.com'><u>mansiruhil99@gmail.com</u></a> :) )</li>
+                <li>● I enjoy building awesome projects that solve practical problems.</li>
+                <li>● And I also have interest in Deep Learning & Computer Vision. </li>
+            </ul>
+        </>
+    )
+}
+function Education() {
+    return (
+        <>
+            <div className=" font-medium relative text-2xl mt-2 md:mt-4 mb-4">
+                Education
+                <div className="absolute pt-px bg-white mt-px top-full w-full">
+                    <div className="bg-white absolute rounded-full p-0.5 md:p-1 top-0 transform -translate-y-1/2 left-full"></div>
+                    <div className="bg-white absolute rounded-full p-0.5 md:p-1 top-0 transform -translate-y-1/2 right-full"></div>
+                </div>
+            </div>
+            <ul className=" w-10/12  mt-4 ml-4 px-0 md:px-1">
+                <li className="list-disc">
+                    <div className=" text-lg md:text-xl text-left font-bold leading-tight">
+                        Banasthali Vidyapith University
+                    </div>
+                    <div className=" text-sm text-gray-400 mt-0.5">2023 - 2027</div>
+                    <div className=" text-sm md:text-base">Computer Science</div>
+                </li>
+            
+                
+            </ul>
+        </>
+    )
+}
+function Skills() {
+    return (
+        <>
+            <div className=" font-medium relative text-2xl mt-2 md:mt-4 mb-4">
+                Technical Skills
+                <div className="absolute pt-px bg-white mt-px top-full w-full">
+                    <div className="bg-white absolute rounded-full p-0.5 md:p-1 top-0 transform -translate-y-1/2 left-full"></div>
+                    <div className="bg-white absolute rounded-full p-0.5 md:p-1 top-0 transform -translate-y-1/2 right-full"></div>
+                </div>
+            </div>
+            <ul className=" tracking-tight text-sm md:text-base w-10/12 emoji-list">
+                <li className=" list-arrow text-sm md:text-base mt-4 leading-tight tracking-tight">
+                    I've worked with a variety of programming languages & frameworks.
+                </li>
+                <li className=" list-arrow text-sm md:text-base mt-4 leading-tight tracking-tight">
+                    <div> My areas of expertise are <strong className="text-ubt-gedit-orange"> Frontend Development, React.js & javascript.</strong></div>
+                </li>
+            </ul>
+            <div className="w-full md:w-10/12 flex mt-4">
+                <div className=" text-sm text-center md:text-base w-1/2 font-bold">Languages & Tools</div>
+                <div className=" text-sm text-center md:text-base w-1/2 font-bold">Frameworks & Libraries</div>
+            </div>
+            <div className="w-full md:w-10/12 flex justify-center items-start font-bold text-center">
+                <div className="px-2 w-1/2">
+                <div className="flex flex-wrap justify-center items-start w-full mt-2">
+    <img className="w-10 h-10 m-2" src="https://skillicons.dev/icons?i=js" alt="JavaScript" />
+    <img className="w-10 h-10 m-2" src="https://skillicons.dev/icons?i=cpp" alt="C++" />
+    <img className="w-10 h-10 m-2" src="https://skillicons.dev/icons?i=python" alt="Python" />
+    <img className="w-10 h-10 m-2" src="https://skillicons.dev/icons?i=html" alt="HTML5" />
+    <img className="w-10 h-10 m-2" src="https://skillicons.dev/icons?i=git" alt="Git" />
+    <img className="w-10 h-10 m-2" src="https://skillicons.dev/icons?i=firebase" alt="Firebase" />
+    <img className="w-10 h-10 m-2" src="https://skillicons.dev/icons?i=figma" alt="Figma"/>
+</div>
+
+
+                </div>
+                <div className="px-2 flex flex-wrap items-start w-1/2">
+                <div className="flex flex-wrap justify-center items-start w-full mt-2">
+    <img className="w-10 h-10 m-2" src="https://skillicons.dev/icons?i=nextjs" alt="Next.js" />
+    <img className="w-10 h-10 m-2" src="https://skillicons.dev/icons?i=react" alt="React" />
+    <img className="w-10 h-10 m-2" src="https://skillicons.dev/icons?i=flutter" alt="Flutter" />
+    <img className="w-10 h-10 m-2" src="https://skillicons.dev/icons?i=taiwindcss" alt="Tailwind CSS" />
+    <img className="w-10 h-10 m-2" src="https://skillicons.dev/icons?i=nodejs" alt="Node.js" />
+</div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+function Projects() {
+    const project_list = [
+        {
+            name: "UbuntuOS Portfolio",
+            date: "Feb 2025",
+            link: "https://github.com/vimistify",
+            description: [
+                "Personal portfolio website of theme Ubuntu 20.04, made using NEXT.js & tailwind CSS",
+            ],
+            domains: ["javascript", "next.js", "tailwindcss"]
+        },
+        {
+            name: "AmbuFlow",
+            date: "September 2024",
+            link: "https://github.com/vimistify/AmbuFlow",
+            description: [
+                "An application that helps users find nearby hospitals and track ambulances in real time.",
+            ],
+            domains: ["javascript", "html", "css"]
+        },
+        {
+            name: "gramminGo",
+            date: "March 2025",
+            link: "https://github.com/vimistify/gramminGo",
+            description: [
+                "An interactive and game based learning app designed to improve spelling, grammar and word usage in English & Hindi.",
+            ],
+            domains: ["javascript", "tailwindcss", "Dockerfile"]
+        },
+        {
+            name: "Image Captioning",
+            date: "Jun 2024",
+            link: "https://github.com/vimistify/Image-Captioning",
+            description: [
+                "An image captioning system that generates descriptive text for images.",
+            ],
+            domains: ["jupyter notebook"]
+        },
+        {
+            name: "Face Recognization",
+            date: "June 2024",
+            link: "https://github.com/vimistify/Face-Recognition",
+            description: [
+                "A face recognition system that detects and identifies faces. ",
+            ],
+            domains: ["python"]
+        },
+        {
+            name: "legato",
+            date: "still-in-dev",
+            link: "https://github.com/vimistify/legato",
+            description: [
+                "An application that transforms legal awareness into engaging, regional language songs, making information accessible to all.",
+            ],
+            domains: ["javascript","taiwind css","react","python","firebase"]
+        },
+        {
+            name: "Mario Run Game Clone",
+            date: "Dec 2024",
+            link: "https://github.com/vimistify/MarioRun",
+            description: [
+                "Mario Run Game Clone.",
+            ],
+            domains: ["javascript", "css", "html"]
+        },
+        {
+            name: "FailUForward",
+            date: "still-in-dev",
+            link: "https://github.com/vimistify/Fail-U-Forward",
+            description: [
+                "Like LinkedIn but for Failures that lead to success. A platform that celebrates learning from failures by sharing epic flops and the valuable lessons learned.",
+            ],
+            domains: ["javascript", "firebase", "react","tailwind css"]
+        },
+        {
+            name: "cyclops",
+            date: "october 2024",
+            link: "https://github.com/vivek9patel/Improve-Codeforces",
+            description: [
+                "Project I have contributed in Open Source. Developer Friendly Kubernetes. ",
+            ],
+            domains: ["go", "javascript","css", "typescript"]
+        },
+    ];
+
+    const tag_colors = {
+        "javascript": "yellow-300",
+        "firebase": "red-600",
+        "firestore": "red-500",
+        "firebase auth": "red-400",
+        "chrome-extension": "yellow-400",
+        "flutter": "blue-400",
+        "dart": "blue-500",
+        "react-native": "purple-500",
+        "html5": "pink-600",
+        "sass": "pink-400",
+        "tensorflow": "yellow-600",
+        "django": "green-600",
+        "python": "green-200",
+        "codeforces-api": "gray-300",
+        "tailwindcss": "blue-300",
+        "next.js": "purple-600"
+    }
+
+    return (
+        <>
+            <div className=" font-medium relative text-2xl mt-2 md:mt-4 mb-4">
+                Projects
+                <div className="absolute pt-px bg-white mt-px top-full w-full">
+                    <div className="bg-white absolute rounded-full p-0.5 md:p-1 top-0 transform -translate-y-1/2 left-full"></div>
+                    <div className="bg-white absolute rounded-full p-0.5 md:p-1 top-0 transform -translate-y-1/2 right-full"></div>
+                </div>
+            </div>
+            <iframe src="https://github.com/sponsors/vimistify/card" title="Sponsor vimistify" className='my-4 w-5/6 md:w-3/4' ></iframe>
+
+            {
+                project_list.map((project, index) => {
+                    const projectNameFromLink = project.link.split('/')
+                    const projectName = projectNameFromLink[projectNameFromLink.length - 1]
+                    return (
+                        <a key={index} href={project.link} target="_blank" rel="noreferrer" className="flex w-full flex-col px-4">
+                            <div className="w-full py-1 px-2 my-2 border border-gray-50 border-opacity-10 rounded hover:bg-gray-50 hover:bg-opacity-5 cursor-pointer">
+                                <div className="flex flex-wrap justify-between items-center">
+                                    <div className='flex justify-center items-center'>
+                                        <div className=" text-base md:text-lg mr-2">{project.name.toLowerCase()}</div>
+                                        <iframe src={`https://ghbtns.com/github-btn.html?user=vimistify&repo=${projectName}&type=star&count=true`} frameBorder="0" scrolling="0" width="150" height="20" title={project.name.toLowerCase()+"-star"}></iframe>
+                                    </div>
+                                    <div className="text-gray-300 font-light text-sm">{project.date}</div>
+                                </div>
+                                <ul className=" tracking-normal leading-tight text-sm font-light ml-4 mt-1">
+                                    {
+                                        project.description.map((desc, index) => {
+                                            return <li key={index} className="list-disc mt-1 text-gray-100">{desc}</li>;
+                                        })
+                                    }
+                                </ul>
+                                <div className="flex flex-wrap items-start justify-start text-xs py-2">
+                                    {
+                                        (project.domains ?
+                                            project.domains.map((domain, index) => {
+                                                return <span key={index} className={`px-1.5 py-0.5 w-max border border-${tag_colors[domain]} text-${tag_colors[domain]} m-1 rounded-full`}>{domain}</span>
+                                            })
+
+                                            : null)
+                                    }
+                                </div>
+                            </div>
+                        </a>
+                    )
+                })
+            }
+        </>
+    )
+}
+function Resume() {
+    return (
+        <iframe className="h-full w-full" src="./files/Mansi_Ruhil_Resume.pdf" title="mansi ruhil resume" frameBorder="0"></iframe>
+    )
+}
